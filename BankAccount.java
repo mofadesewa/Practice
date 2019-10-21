2f8de0513aba363518fca048d977bab94262b3c3
Mofadesewa Atanda <44592706+mofadesewa@users.noreply.github.com>
2019-10-20 23:36:42 -0500

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -104,3 +104,42 @@ public class BankAccount {
 			}
 		}
 }
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAccount Alice = new BankAccount("Alice", 100, "savings", 50);
+		BankAccount Bob = new BankAccount("Bob", 350, "checkings", 30);
+		
+		Bob.getBalance();
+		System.out.println("");
+
+
+		Bob.withDraw(200);
+
+		
+		Bob.getBalance();
+		System.out.println("");
+		
+
+		Alice.getBalance();
+		System.out.println("");
+		
+		Alice.deposit(50);
+		System.out.println("");
+
+		Alice.getBalance();
+		System.out.println("");
+
+		Bob.getBalance();
+		System.out.println("");
+
+		Bob.withDraw(1000);
+		System.out.println("");
+
+		Bob.transfer(Alice, 75);
+		System.out.println("");
+
+	}
+
+}
